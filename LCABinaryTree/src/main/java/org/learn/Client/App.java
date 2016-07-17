e1cd800a84ee414768271aeef995b89eb0843f94
makeinjava <makeinjava@gmail.com>
2016-07-17 14:03:17 +0530

    Update binary tree element

--- LCABinaryTree/src/main/java/org/learn/Client/App.java
+++ LCABinaryTree/src/main/java/org/learn/Client/App.java
@@ -3,28 +3,26 @@ package org.learn.Client;
 import org.learn.Question.LCA;
 import org.learn.Question.Node;
 
-public class App 
-{
-	public static void main( String[] args )
-    {		
+public class App {
+	public static void main(String[] args) {
 		// root level 0
-		Node A = Node.createNode(100);
+		Node A = Node.createNode(50);
 		// Level 1
-		Node B = Node.createNode(50);
-		Node C = Node.createNode(150);
+		Node B = Node.createNode(25);
+		Node C = Node.createNode(75);
 		// Level 2
-		Node D = Node.createNode(25);
-		Node E = Node.createNode(80);
-		Node F = Node.createNode(125);
-		Node G = Node.createNode(160);
-		
+		Node D = Node.createNode(15);
+		Node E = Node.createNode(40);
+		Node F = Node.createNode(60);
+		Node G = Node.createNode(95);
+
 		// Level 3
 		Node H = Node.createNode(10);
-		Node I = Node.createNode(30);
-		Node J = Node.createNode(60);
-		Node K = Node.createNode(90);
-		Node L = Node.createNode(110);
-		Node M = Node.createNode(140);
+		Node I = Node.createNode(20);
+		Node J = Node.createNode(30);
+		Node K = Node.createNode(45);
+		Node L = Node.createNode(55);
+		Node M = Node.createNode(65);
 
 		// connect Level 0 and 1
 		A.left = B;
@@ -41,14 +39,20 @@ public class App
 		E.right = K;
 		F.left = L;
 		F.right = M;
-		
-    	Node lca = LCA.lca(A, D, H);
-    	System.out.println("LCA of Node D ("+D.data + ") and Node H ("+ H.data +") is  : "+ lca.data);  
-    	
-    	lca = LCA.lca(A, I, K);
-    	System.out.println("LCA of Node I ("+I.data + ") and Node K ("+ K.data +") is  : "+ lca.data);  
-		
-    	lca = LCA.lca(A, I, M);
-    	System.out.println("LCA of Node I ("+I.data + ") and Node M ("+ M.data +") is : "+ lca.data);        
-    }
+
+		Node lca = LCA.lca(A, D, H);
+		String message = String.format("1. LCA[Node D (%d) & Node H (%d)] = %d",
+													D.data,H.data,lca.data);
+		System.out.println(message);
+
+		lca = LCA.lca(A, I, K);
+		message = String.format("2. LCA[Node I (%d) & Node K (%d)] = %d", 
+													I.data, K.data, lca.data);
+		System.out.println(message);
+
+		lca = LCA.lca(A, I, M);
+		message = String.format("3. LCA[Node I (%d) & Node M (%d)] = %d", 
+													I.data, M.data, lca.data);
+		System.out.println(message);
+	}
 }
--- LCABinaryTree/src/main/java/org/learn/Question/LCA.java
+++ LCABinaryTree/src/main/java/org/learn/Question/LCA.java
@@ -2,21 +2,22 @@
 package org.learn.Question;
 
 public class LCA {
-	public static Node lca(Node root, Node A ,Node B) {
-		if(null == root) {
+	public static Node lca(Node root, Node node1, Node node2) {
+		if (null == root) {
 			return root;
 		}
-		if(root == A || root == B) {
+		if (root == node1 || root == node2) {
 			return root;
 		}
-		Node left = lca(root.left, A, B);
-		Node right = lca(root.right, A, B);
-		
-		if(left != null && right != null) {
+		Node left = lca(root.left, node1, node2);
+		Node right = lca(root.right, node1, node2);
+
+		if (left != null && right != null) {
 			return root;
 		}
-		if(left != null)
+		if (left != null)
 			return left;
-		else return right;
+		else
+			return right;
 	}
 }
